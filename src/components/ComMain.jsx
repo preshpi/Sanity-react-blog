e68dd14cd3cc9cb38952664f2aaa1786b8432e21
preshpi <egwuenuprecious7@gamil.com>
2023-04-15 11:14:31 +0100

    fix: removed newsletter

--- src/components/ComMain.jsx
+++ src/components/ComMain.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Header from "./Header";
-import Newsletter from "./Newsletter";
 
 function ComMain() {
   return (
     <div>
       <Navbar />
       <Header />
-      <Newsletter />
     </div>
   );
 }
